165ae77c16afff054518f766ad9e717b72a9b50b
goruchan <ppcfr481@yahoo.co.jp>
2023-11-26 22:55:17 +0900

    clean 重複コードを削除

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -60,10 +60,8 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    if current_user.premium
-      params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
-    else
-      params.require(:blog).permit(:title, :content, :secret)
-    end
+    permit_params = %i[title content secret]
+    permit_params.push(:random_eyecatch) if current_user.premium
+    params.require(:blog).permit(permit_params)
   end
 end
